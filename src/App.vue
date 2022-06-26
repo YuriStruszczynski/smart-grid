48fea485ec911fa192ed3f3665c9db594d1adbc0
ystruszczynski <yuri.struszczynski@googlemail.com>
2022-06-26 23:03:34 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,125 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="container">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+        <span class="fs-4">Simple header</span>
+      </a>
+
+      <ul class="nav nav-pills">
+        <li class="nav-item">
+            <RouterLink to="/" class="nav-link">Home</RouterLink>
+        </li>
+        <li class="nav-item">
+            <RouterLink to="/about" class="nav-link">About</RouterLink>
+        </li>
+      </ul>
+    </header>
+  </div>
+
+  <RouterView />
+  
+</template>
+
+<style>
+@import '@/assets/base.css';
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+a,
+.green {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  body {
+    display: flex;
+    place-items: center;
+  }
+
+  #app {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 0 2rem;
+  }
+
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,236 @@
+<template>
+{{rows}}
+<br/><br/><br/>
+  <template v-for="(row, rowIndex) in rows" :key="row.id">
+    <div class="container">
+      <div class="row shadow-row"> 
+        <div class="col" v-for="index in 12" :key="index" @drop="drop($event, rowIndex, index)" @dragover="allowDrop($event)">
+          <div>{{index}}</div>	
+        </div>
+      </div>
+      <div class="row align-items-start mark-row grabbable">
+        <div :class="col.cs" v-for="(col, colIndex) in row.cols" :key="col.id" draggable="true" @dragstart="dragStart($event, col)">
+          <div>{{col.cs}} S:{{col.start}} E:{{col.end}}</div>		
+        </div>
+      </div>
+    </div>
+  </template>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  data() {
+    return {
+      rowRepo: [],
+      forceRecomputeDealListsCounter: 0
+    };
+  },
+  computed: {
+    rows: function() {
+        this.forceRecomputeDealListsCounter;
+		    let rows = this.rowRepo.getRows();
+
+        //calculate bootstrap classes
+        rows.forEach(row => {
+          let lastEnd = 1;
+          let cols = row.cols;
+          cols.sort((a, b) => a.start - b.start);
+
+          cols.forEach(col => {
+            let cs = "";
+            if(col.start>1 && col.start!=(lastEnd+1)){
+              let width = col.end-col.start+1;
+              cs = "offset-"+(lastEnd-1)+" col-"+width.toString();
+            }else if(col.start>1){
+              let width = col.end-col.start+1;
+              cs = "offset-"+(lastEnd-1)+" col-"+width.toString();
+            }else{
+              let width = col.end-col.start+1;
+              cs = "col-"+width.toString();
+            }
+            lastEnd = col.end;
+            col.cs = cs+" lastEnd-"+lastEnd;
+          });
+        });
+
+        console.log(rows);
+        return Object.assign({}, rows);
+    },
+  },
+  methods: {
+    dragStart: function(event, col) {
+      event.dataTransfer.setData("Text", col.id);
+      console.log(col.id);
+    },
+    allowDrop: function(event) {
+      event.preventDefault();
+    },
+    drop: function(event, rowId, start) {
+      event.preventDefault();
+      var colId = event.dataTransfer.getData("Text");
+      console.log("moveCol",colId, rowId);
+      this.rowRepo.moveCol(colId, rowId, start);
+      this.forceRecomputeDealListsCounter++;
+    }
+  },
+  watch: {
+  },
+  created: function() {
+    this.rowRepo = new RowRepo();
+  },
+  mounted: function() {
+  },
+  updated: function() {
+  } 
+};
+
+class RowRepo {
+		constructor() {
+			this.rows = [
+        {
+          id:4,
+          cols: [
+            {id: 2, start:2, end:3, content:'test', cs:''},
+          ]
+        },
+        {
+          id:0,
+          cols: [
+            {id: 0, start:1, end:2, content:'test', cs:''},
+            {id: 1, start:4, end:6, content:'test2', cs:''}
+          ]
+        },
+        {
+          id:1,
+          cols: [
+            {id: 2, start:1, end:2, content:'test', cs:''},
+            {id: 3, start:3, end:5, content:'test2', cs:''},
+            {id: 4, start:6, end:12, content:'test3', cs:''}
+          ]
+        },
+        {
+          id:3,
+          cols: [
+            {id: 2, start:4, end:5, content:'test', cs:''},
+          ]
+        },
+      ];
+		}
+
+		getRows() {
+			let arrayCopy = this.rows.map(i => Object.assign({}, i));
+			return arrayCopy;
+		}
+
+		getRowById(id) {
+			let row = this.rows.find((i) => i.id == id);
+			return Object.assign({}, row);
+		}
+
+		getRowsByProject(project) {
+			let rows = this.rows.filter((i) => i.project == project);
+			return Object.assign({}, rows);
+		}
+
+		getRowByLeadStatus(searchValue) {
+			return this.rows.filter(row => {
+				if (row.lead_status != null)
+					return row.lead_status.toLowerCase().indexOf(searchValue.toLowerCase()) > -1
+			})
+		}
+
+		updateRow(id, data) {
+			let toChange = this.rows.findIndex(i => i.id == id);
+			this.rows[toChange] = Object.assign(this.rows[toChange], data);
+		}
+
+		deleteById(id) {
+			let toDelete = this.rows.findIndex(i => i.id == id);
+			this.rows.splice(toDelete, 1);
+		}
+
+		createEmpty() {
+			let emptyRows = {};
+			return emptyRows;
+		}
+
+		add(row) {
+			//append
+			//this.rows.push();
+			//prepend
+			this.rows = [row, ...this.rows];
+		}
+
+    findRowForColId(colId){
+      let foundIndex = -1;
+      this.rows.forEach((row, index) => {
+        let col = row.cols.find((i) => i.id == colId);
+        if(col){
+          console.log("found", index);
+          foundIndex = index;
+        }else{
+          console.log("not found");
+        }
+      });
+      return foundIndex;
+    }
+
+	  moveCol(colId, rowId, start) { 
+      console.log("before",this.rows);
+      let prevRowIndex = this.findRowForColId(colId);
+      
+      console.log("prevRowIndex",prevRowIndex);
+      if(prevRowIndex==-1) return;
+
+      //presave col
+			let col = this.rows[prevRowIndex].cols.find(i => i.id == colId);
+
+      //change start
+      let width = col.end-col.start;
+      col.start = start;
+      col.end = start+width;
+      
+      //delete col
+			let toDelete = this.rows[prevRowIndex].cols.findIndex(i => i.id == colId);
+			this.rows[prevRowIndex].cols.splice(toDelete, 1);
+
+      //add col
+			this.rows[rowId].cols = [col, ...this.rows[rowId].cols];
+      
+      console.log("after",this.rows);
+
+		}
+	}
+
+</script>
+
+<style>
+.container{
+  margin-bottom: 4rem;
+}
+.mark-row > div{
+  min-height: 4rem;
+}
+.mark-row > div > div{
+  padding:10px;
+  border:1px solid #999;
+  min-height: 4rem;
+  background-color: #999;
+  opacity: 0.9;
+}
+
+.shadow-row{
+  top:-25px;
+  position: absolute;
+  width: 100%;
+}
+
+.shadow-row > div > div{
+  border:1px solid #333;
+  min-height: 4rem;
+}
+
+</style>
