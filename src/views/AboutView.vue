b1e19d5585b08b0b5c76f0c73bface83e3de9171
ystruszczynski <yuri.struszczynski@googlemail.com>
2022-07-10 23:19:37 +0200

    grid ready, modules in sidenavi dragable

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,20 +1,38 @@
 <template>
-{{rows}}
-<br/><br/><br/>
-  <template v-for="(row, rowIndex) in rows" :key="row.id">
-    <div class="container">
-      <div class="row shadow-row"> 
-        <div class="col" v-for="index in 12" :key="index" @drop="drop($event, rowIndex, index)" @dragover="allowDrop($event)">
-          <div>{{index}}</div>	
+<!--{{rows}}-->
+<br/>
+<div class="row">
+  <div class="col-10">
+    <template v-for="(row, rowIndex) in rows" :key="row.id">
+      <div class="container">
+        <div class="row shadow-row"> 
+          <div class="col" v-for="index in 12" :key="index" @drop="drop($event, rowIndex, index)" @dragover="allowDrop($event)">
+            <div>{{index}}</div>	
+          </div>
+        </div>
+        <div class="row align-items-start mark-row grabbable">
+          <div :class="col.cs" v-for="(col, colIndex) in row.cols" :key="col.id" draggable="true" @dragstart="dragStart($event, col)">
+            <div>{{col.content}}<!--{{col.cs}} S:{{col.start}} E:{{col.end}}--></div>		
+          </div>
         </div>
       </div>
-      <div class="row align-items-start mark-row grabbable">
-        <div :class="col.cs" v-for="(col, colIndex) in row.cols" :key="col.id" draggable="true" @dragstart="dragStart($event, col)">
-          <div>{{col.cs}} S:{{col.start}} E:{{col.end}}</div>		
-        </div>
+    </template>
+  </div>
+  <div class="col-2">
+
+    <div class="card">
+      <div class="card-header">
+        Module
       </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item" draggable="true" @dragstart="dragNew($event, 'Text')">Text</li>
+        <li class="list-group-item" draggable="true" @dragstart="dragNew($event, 'Bild')">Bild</li>
+        <li class="list-group-item" draggable="true" @dragstart="dragNew($event, 'Video')">Video</li>
+      </ul>
     </div>
-  </template>
+
+  </div>
+</div>
 </template>
 
 <script>
@@ -35,21 +53,24 @@ export default {
 
         //calculate bootstrap classes
         rows.forEach(row => {
-          let lastEnd = 1;
+          let lastEnd = 0;
           let cols = row.cols;
           cols.sort((a, b) => a.start - b.start);
 
           cols.forEach(col => {
-            let cs = "";
-            if(col.start>1 && col.start!=(lastEnd+1)){
+            let cs = "ID:"+col.id+" ";
+            if(col.start==1){
               let width = col.end-col.start+1;
-              cs = "offset-"+(lastEnd-1)+" col-"+width.toString();
+              cs += "1) col-"+width.toString();
+            }else if(col.start>1 && lastEnd<col.start){
+              let width = col.end-col.start+1;
+              cs += "2) offset-"+(col.start-lastEnd-1)+" col-"+width.toString();
             }else if(col.start>1){
               let width = col.end-col.start+1;
-              cs = "offset-"+(lastEnd-1)+" col-"+width.toString();
+              cs += "3) offset-"+(lastEnd-1)+" col-"+width.toString();
             }else{
               let width = col.end-col.start+1;
-              cs = "col-"+width.toString();
+              cs += "4) col-"+width.toString();
             }
             lastEnd = col.end;
             col.cs = cs+" lastEnd-"+lastEnd;
@@ -61,8 +82,12 @@ export default {
     },
   },
   methods: {
+    dragNew: function(event, type) {
+      event.dataTransfer.setData("type", type);
+      console.log(type);
+    },
     dragStart: function(event, col) {
-      event.dataTransfer.setData("Text", col.id);
+      event.dataTransfer.setData("col.id", col.id);
       console.log(col.id);
     },
     allowDrop: function(event) {
@@ -70,9 +95,19 @@ export default {
     },
     drop: function(event, rowId, start) {
       event.preventDefault();
-      var colId = event.dataTransfer.getData("Text");
-      console.log("moveCol",colId, rowId);
-      this.rowRepo.moveCol(colId, rowId, start);
+
+      var type = event.dataTransfer.getData("type");
+      if(type){
+        console.log("newCol",type, rowId);
+        this.rowRepo.newCol(type, rowId, start);
+      }
+
+      var colId = event.dataTransfer.getData("col.id");
+      if(colId){
+        console.log("moveCol",colId, rowId);
+        this.rowRepo.moveCol(colId, rowId, start);
+      }
+
       this.forceRecomputeDealListsCounter++;
     }
   },
@@ -91,30 +126,23 @@ class RowRepo {
 		constructor() {
 			this.rows = [
         {
-          id:4,
-          cols: [
-            {id: 2, start:2, end:3, content:'test', cs:''},
-          ]
-        },
-        {
-          id:0,
-          cols: [
-            {id: 0, start:1, end:2, content:'test', cs:''},
-            {id: 1, start:4, end:6, content:'test2', cs:''}
-          ]
-        },
-        {
           id:1,
           cols: [
-            {id: 2, start:1, end:2, content:'test', cs:''},
-            {id: 3, start:3, end:5, content:'test2', cs:''},
-            {id: 4, start:6, end:12, content:'test3', cs:''}
+            {id: 3, start:1, end:2, content:'Text', cs:''},
+            {id: 4, start:3, end:5, content:'Text', cs:''},
+            {id: 5, start:6, end:12, content:'Video', cs:''}
+          ]
+        },
+        {
+          id:2,
+          cols: [
+            {id: 0, start:2, end:3, content:'Bild', cs:''},
           ]
         },
         {
           id:3,
           cols: [
-            {id: 2, start:4, end:5, content:'test', cs:''},
+            {id: 6, start:4, end:5, content:'Video', cs:''},
           ]
         },
       ];
@@ -178,6 +206,26 @@ class RowRepo {
       return foundIndex;
     }
 
+    getMaxId(){
+      const ids = this.rows.map(row => {
+        return row.id;
+      });
+      return Math.max(...ids);
+    }
+
+	  newCol(type, rowId, start) { 
+      //find max id
+      let newId = this.getMaxId();
+
+      //create col
+			let col = {id: newId, start:start, end:start+1, content:type, cs:''};
+
+      //add col
+			this.rows[rowId].cols = [col, ...this.rows[rowId].cols];
+		}
+
+    
+
 	  moveCol(colId, rowId, start) { 
       console.log("before",this.rows);
       let prevRowIndex = this.findRowForColId(colId);
@@ -223,14 +271,12 @@ class RowRepo {
 }
 
 .shadow-row{
-  top:-25px;
-  position: absolute;
-  width: 100%;
+  top:4rem;
 }
 
 .shadow-row > div > div{
   border:1px solid #333;
-  min-height: 4rem;
+  min-height: 6rem;
 }
 
 </style>
